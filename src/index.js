abfcfc961b111db11a62ee11c929857afb556c79
Abdullah <abdullah.abrahams@4-sure.net>
2022-04-23 11:27:06 +0200

    init

---
+++ src/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import SortableComponent from './components/App';
+import './index.css';
+
+ReactDOM.render(
+    <SortableComponent />,
+    document.getElementById("root")
+);
